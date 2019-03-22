c738e49df6eabe0c01004e17f44b083135fb971a
qianqianya <1683472461@qq.com>
2019-03-22 19:25:40 +0800

    单点

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -14,17 +14,27 @@ class UserController extends Controller
     {
         $email=$request->input('u_email');
         $pwd=$request->input('u_pwd');
+        $type=$request->input('type');
         $where=[
-        'u_email'=>$email
+            'u_email'=>$email
         ];
         $u_pwd=UserModel::where($where)->first();
         if($u_pwd){
             if($u_pwd['u_pwd']==$pwd){
                 $token = substr(md5(time().mt_rand(1,99999)),10,10);
                 //记录web记录token
-                $redis_key_web_token='str:u:token:web'.$u_pwd->u_id;
-                Redis::set($redis_key_web_token,$token);
-                Redis::expire($redis_key_web_token,86400);
+                if($type==1){
+                    $redis_key_web_token='str:u:token:web'.$u_pwd->u_id;
+                    Redis::delete();
+                    Redis::set($redis_key_web_token,$token);
+                    Redis::expire($redis_key_web_token,86400);
+                }elseif($type==2){
+                    $redis_key_web_token='str:u:token:app'.$u_pwd->u_id;
+                    Redis::delete();
+                    Redis::set($redis_key_web_token,$token);
+                    Redis::expire($redis_key_web_token,86400);
+                }
+
                 $data=[
                 'token'=>$token
                 ];
